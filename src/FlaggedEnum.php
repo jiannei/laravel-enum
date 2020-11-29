ac488cd08889acdc20842f4a656e74ffaf2d2e5b
Jiannei <Jiannei@users.noreply.github.com>
2020-11-29 09:06:02 +0000

    Apply fixes from StyleCI

--- src/FlaggedEnum.php
+++ src/FlaggedEnum.php
@@ -1,7 +1,15 @@
 <?php
 
-namespace Jiannei\Enum\Laravel;
+/*
+ * This file is part of the Jiannei/laravel-enum.
+ *
+ * (c) Jiannei <longjian.huang@foxmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
+namespace Jiannei\Enum\Laravel;
 
 use Jiannei\Enum\Laravel\Exceptions\InvalidEnumValueException;
 
